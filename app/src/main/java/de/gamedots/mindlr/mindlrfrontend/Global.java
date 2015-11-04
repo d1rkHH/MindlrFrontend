98a01c56815dfe75748ea60cce75329561ea0603
Dirk Löwenstrom <d.loewenstrom@web.de>
2015-11-04 14:31:14 +0100

    added METHOD_VERIFY

--- app/src/main/java/de/gamedots/mindlr/mindlrfrontend/Global.java
+++ app/src/main/java/de/gamedots/mindlr/mindlrfrontend/Global.java
@@ -27,6 +27,8 @@ public final class Global {
     public static final String METHOD_POST = "POST";
     public static final String METHOD_GET = "GET";
 
+    public static final String METHOD_VERIFY = "VERIFY_TOKEN";
+
 
 
     public static class Categories {
