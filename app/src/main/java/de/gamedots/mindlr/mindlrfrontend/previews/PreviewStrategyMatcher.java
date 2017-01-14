70b5b6dfb92ce97d88abef01b101cf5fd6678e09
MaxMello <max.mails@ymail.com>
2017-01-14 03:31:45 +0100

    Added stubs for preview strategy handling

---
+++ app/src/main/java/de/gamedots/mindlr/mindlrfrontend/previews/PreviewStrategyMatcher.java
@@ -0,0 +1,19 @@
+package de.gamedots.mindlr.mindlrfrontend.previews;
+
+import java.util.List;
+
+import de.gamedots.mindlr.mindlrfrontend.previews.strategy.PreviewStrategy;
+
+/**
+ * Singleton
+ * Created by max on 14.01.17.
+ */
+
+public class PreviewStrategyMatcher {
+
+    private List<PreviewStrategy> _previewStrategies;
+
+    public PreviewStrategy matchStrategy(List<String> urls){
+        return null;
+    }
+}
